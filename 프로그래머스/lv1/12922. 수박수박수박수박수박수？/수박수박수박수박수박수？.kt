29fcaee1e336be683fbe84f97e86ec723227d08f
Lst-1995-kotlin <120715142+Lst-1995-kotlin@users.noreply.github.com>
2023-06-05 15:42:51 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.12 ms, Memory: 62.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.kt
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.kt
@@ -1,10 +1,10 @@
 class Solution {
     fun solution(n: Int): String {
-        var answer = ""
+        var answer = StringBuilder()
         for(i in 0 .. n-1){
-            if(i % 2 == 0) answer += "수" else answer += "박"
-            
+            if(i % 2 == 0) answer.append("수") else answer.append("박")
+
         }
-        return answer
+        return answer.toString()
     }
 }
