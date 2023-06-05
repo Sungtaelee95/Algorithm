1b23eec2f14c02a8b3505ad06a4aa7f0a1ca38e8
Lst-1995-kotlin <120715142+Lst-1995-kotlin@users.noreply.github.com>
2023-06-05 16:01:55 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 2436 ms, Memory: 174540 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.kt
@@ -0,0 +1,34 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.util.*
+import kotlin.math.abs
+
+fun main() {
+//    val s = System.currentTimeMillis()
+    var br = BufferedReader(InputStreamReader(System.`in`))
+    var bw = BufferedWriter(OutputStreamWriter(System.out))
+    var st = StringTokenizer(br.readLine())
+    val N = st.nextToken().toInt() // 자료의 개수
+    //var sb = StringBuilder()
+
+    var arr = IntArray(N){0}
+    repeat(N){
+        st = StringTokenizer(br.readLine())
+        arr[it] = st.nextToken().toInt()
+    }
+    arr.sorted().forEach{
+        bw.appendLine(it.toString())
+    }
+    
+
+    bw.flush()
+    bw.close()
+
+//   val e = System.currentTimeMillis()
+//   println((e-s)/1000.0)
+
+}
+
+
