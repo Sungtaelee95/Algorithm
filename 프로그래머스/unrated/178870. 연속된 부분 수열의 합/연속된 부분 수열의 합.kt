318aefd0a33719cbc21898df4cb050a88853e692
Lst-1995-kotlin <120715142+Lst-1995-kotlin@users.noreply.github.com>
2023-06-05 14:54:42 +0900

    [unrated] Title: 연속된 부분 수열의 합, Time: 0.02 ms, Memory: 62.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.kt
@@ -0,0 +1,27 @@
+class Solution {
+    fun solution(sequence: IntArray, k: Int): IntArray {
+        var answer: IntArray = intArrayOf(0,1000000001)
+
+        var sum = sequence[0]
+        var left = 0
+        var right = 0
+
+        while(left < sequence.size && right < sequence.size){
+            if(sum < k){
+                right++
+                if(right < sequence.size) sum += sequence[right]
+            } else if(sum > k){
+                sum -= sequence[left]
+                left++
+            } else {
+                if(answer[1]-answer[0] > right - left){
+                    answer = intArrayOf(left,right)
+                }
+                sum -= sequence[left]
+                left++
+            }
+        }
+
+        return answer
+    }
+}
