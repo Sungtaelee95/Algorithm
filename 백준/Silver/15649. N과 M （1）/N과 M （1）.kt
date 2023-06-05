aa02a94540cc72c00e755c9eec4a3597efa46a67
Lst-1995-kotlin <120715142+Lst-1995-kotlin@users.noreply.github.com>
2023-06-05 16:07:12 +0900

    [Silver III] Title: N과 M (1), Time: 232 ms, Memory: 32132 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.kt
@@ -0,0 +1,45 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.util.*
+
+lateinit var visit : BooleanArray
+lateinit var bw : BufferedWriter
+var N = 0
+var M = 0
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    bw = BufferedWriter(OutputStreamWriter(System.out))
+    var st = StringTokenizer(br.readLine())
+
+    N = st.nextToken().toInt()
+    M = st.nextToken().toInt()
+
+    visit = BooleanArray(N+1)
+
+    for(i in 1 .. N){
+        visit[i] = true
+        dfs(i.toString(),1)
+        visit[i] = false
+    }
+
+    bw.flush()
+    bw.close()
+}
+
+fun dfs(num: String,count: Int){
+    if(count == M){
+        bw.appendLine(num)
+        return
+    }
+
+    for(i in 1 .. N){
+        if(visit[i])continue
+        visit[i] = true
+        dfs("$num $i",count+1)
+        visit[i] = false
+    }
+
+}
