8da85643f016683499886630f0bc118025a7488e
Lst-1995-kotlin <120715142+Lst-1995-kotlin@users.noreply.github.com>
2023-06-05 15:31:29 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 10.88 ms, Memory: 66.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.kt
@@ -0,0 +1,10 @@
+class Solution {
+    fun solution(strings: Array<String>, n: Int): Array<String> {
+        
+        return strings.also {
+            it.sort()
+            it.sortBy {it[n]}
+        }
+           
+    }
+}
