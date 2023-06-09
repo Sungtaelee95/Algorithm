e1c908d33e23f9b6c5320d9780db02c54cdb6f8d
Lst-1995-kotlin <120715142+Lst-1995-kotlin@users.noreply.github.com>
2023-06-10 02:54:17 +0900

    [Silver I] Title: 삼각 그래프, Time: 400 ms, Memory: 51992 KB -BaekjoonHub

--- 백준/Silver/4883. 삼각 그래프/삼각 그래프.kt
+++ 백준/Silver/4883. 삼각 그래프/삼각 그래프.kt
@@ -28,36 +28,31 @@ fun main(){
         resultMap[0][1] = map[0][1]
         resultMap[0][2] = map[0][1] + map[0][2]
 
-//        resultMap[1][0] = resultMap[0][1] + map[1][0]
-//        resultMap[1][1] = Math.min(resultMap[0][1] + map[1][1] , resultMap[0][2] + map[1][1])
-//        resultMap[1][2] = Math.min(resultMap[0][1] + map[1][2] , resultMap[0][2] + map[1][2])
-
-
         for(row in 1 .. N-1){
             for(col in 0 .. 2){
 
                 if(col == 0){
                     if(row == 1){
-                        resultMap[row][col] = resultMap[row-1][col+1] + map[row][col]
+                        resultMap[row][col] = resultMap[0][1] + map[1][0]
                         continue
                     }
-                    resultMap[row][col] = resultMap[row-1][col] + map[row][col]
-                    resultMap[row][col] = Math.min(resultMap[row][col], resultMap[row-1][col+1] + map[row][col])
+                    resultMap[row][0] = resultMap[row-1][0] + map[row][0]
+                    resultMap[row][0] = Math.min(resultMap[row][0], resultMap[row-1][1] + map[row][0])
                 }
 
                 if(col == 1){
 
-                    resultMap[row][col] = resultMap[row][0] + map[row][col] // 좌측에서 오는 거
-                    resultMap[row][col] = Math.min(resultMap[row][col], resultMap[row-1][col] + map[row][col])
-                    resultMap[row][col] = Math.min(resultMap[row][col], resultMap[row-1][2] + map[row][col])
-                    if(row != 1) resultMap[row][col] = Math.min(resultMap[row][col], resultMap[row-1][0] + map[row][col])
+                    resultMap[row][1] = resultMap[row][0] + map[row][1] // 좌측에서 오는 거
+                    resultMap[row][1] = Math.min(resultMap[row][1], resultMap[row-1][1] + map[row][1])
+                    resultMap[row][1] = Math.min(resultMap[row][1], resultMap[row-1][2] + map[row][1])
+                    if(row != 1) resultMap[row][1] = Math.min(resultMap[row][1], resultMap[row-1][0] + map[row][1])
                 }
 
                 if(col == 2){
 
-                    resultMap[row][col] = resultMap[row][col-1] + map[row][col] // 좌측에서 오는 거
-                    resultMap[row][col] = Math.min(resultMap[row][col], resultMap[row-1][col-1] + map[row][col])
-                    resultMap[row][col] = Math.min(resultMap[row][col], resultMap[row-1][col] + map[row][col])
+                    resultMap[row][2] = resultMap[row][1] + map[row][2] // 좌측에서 오는 거
+                    resultMap[row][2] = Math.min(resultMap[row][2], resultMap[row-1][1] + map[row][2])
+                    resultMap[row][2] = Math.min(resultMap[row][2], resultMap[row-1][2] + map[row][2])
                 }
 
             }
