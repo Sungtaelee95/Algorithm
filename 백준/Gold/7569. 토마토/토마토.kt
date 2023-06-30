9bfd4285529b1c9e45f3d0eb621e7242847c1e79
Lst-1995-kotlin <120715142+Lst-1995-kotlin@users.noreply.github.com>
2023-07-01 02:23:12 +0900

    [Gold V] Title: 토마토, Time: 864 ms, Memory: 113292 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.kt
+++ 백준/Gold/7569. 토마토/토마토.kt
@@ -16,14 +16,6 @@ val highArr = intArrayOf(-1, 1)
 val rowArr = intArrayOf(0, 0, -1, 1)
 val colArr = intArrayOf(-1, 1, 0, 0)
 
-var mapCol = 0
-var mapRow = 0
-var mapHigh = 0
-val dq = ArrayDeque<Info>()
-
-lateinit var map : Array<Array<IntArray>>
-lateinit var dayMap : Array<Array<IntArray>>
-
 fun main(){
     val br = BufferedReader(InputStreamReader(System.`in`))
     val bw = BufferedWriter(OutputStreamWriter(System.out))
@@ -33,12 +25,12 @@ fun main(){
     // dfs 실행 중에 1을 만날 경우는 넘김.
     // 마지막에 0이 남아있다면 -1 반환
 
-    mapCol = st.nextToken().toInt()
-    mapRow = st.nextToken().toInt()
-    mapHigh = st.nextToken().toInt()
+    val mapCol = st.nextToken().toInt()
+    val mapRow = st.nextToken().toInt()
+    val mapHigh = st.nextToken().toInt()
 
-    map = Array(mapHigh){Array(mapRow){IntArray(mapCol)}}
-    dayMap = Array(mapHigh){Array(mapRow){IntArray(mapCol){1_000_001}}}
+    val map = Array(mapHigh){Array(mapRow){IntArray(mapCol)}}
+    val dayMap = Array(mapHigh){Array(mapRow){IntArray(mapCol){1_000_001}}}
 
     for(high in 0 until mapHigh){
         for(row in 0 until mapRow){
@@ -49,16 +41,44 @@ fun main(){
         }
     }
 
+    val dq = ArrayDeque<Info>()
+
     for(high in 0 until mapHigh){
         for(row in 0 until mapRow){
             for(col in 0 until mapCol){
                 if(map[high][row][col] == 1) {
                     dayMap[high][row][col] = 0
                     dq.add(Info(high, row, col, 0))
-                    bfs()
                 }
             }
         }
+    }
+
+    while(!dq.isEmpty()){
+        val now = dq.pollFirst()
+
+        for(high in highArr){
+            val nextHigh = now.high + high
+
+            if(nextHigh < 0 || nextHigh >= mapHigh) continue
+            if(map[nextHigh][now.row][now.col] == -1) continue
+            if(dayMap[nextHigh][now.row][now.col] <= dayMap[now.high][now.row][now.col]+1) continue
+
+            dayMap[nextHigh][now.row][now.col] = now.time+1
+            dq.addLast(Info(nextHigh, now.row, now.col, now.time+1))
+        }
+
+        for(i in 0 .. 3){
+            val nextRow = now.row + rowArr[i]
+            val nextCol = now.col + colArr[i]
+
+            if(nextRow < 0 || nextCol < 0 || nextRow >= mapRow || nextCol >= mapCol) continue
+            if(map[now.high][nextRow][nextCol] == -1) continue
+            if(dayMap[now.high][nextRow][nextCol] <= dayMap[now.high][now.row][now.col]+1) continue
+            dayMap[now.high][nextRow][nextCol] = now.time+1
+            dq.addLast(Info(now.high, nextRow, nextCol, now.time+1))
+        }
+
     }
 
     var result = 0
@@ -80,35 +100,5 @@ fun main(){
 
     bw.flush()
     bw.close()
-    
-}
 
-fun bfs(){
-
-    while(!dq.isEmpty()){
-        val now = dq.pollFirst()
-        
-        for(high in highArr){
-            val nextHigh = now.high + high
-
-            if(nextHigh < 0 || nextHigh >= mapHigh) continue
-            if(map[nextHigh][now.row][now.col] == -1) continue
-            if(dayMap[nextHigh][now.row][now.col] <= dayMap[now.high][now.row][now.col]+1) continue
-
-            dayMap[nextHigh][now.row][now.col] = now.time+1
-            dq.addLast(Info(nextHigh, now.row, now.col, now.time+1))
-        }
-        
-        for(i in 0 .. 3){
-            val nextRow = now.row + rowArr[i]
-            val nextCol = now.col + colArr[i]
-
-            if(nextRow < 0 || nextCol < 0 || nextRow >= mapRow || nextCol >= mapCol) continue
-            if(map[now.high][nextRow][nextCol] == -1) continue
-            if(dayMap[now.high][nextRow][nextCol] <= dayMap[now.high][now.row][now.col]+1) continue
-            dayMap[now.high][nextRow][nextCol] = now.time+1
-            dq.addLast(Info(now.high, nextRow, nextCol, now.time+1))
-        }
-        
-    }
 }
