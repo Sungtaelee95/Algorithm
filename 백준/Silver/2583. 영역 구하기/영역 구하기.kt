d02068a84f18eca20b7ef3b5bc34f82639980766
Lst-1995-kotlin <120715142+Lst-1995-kotlin@users.noreply.github.com>
2023-07-01 00:28:01 +0900

    [Silver I] Title: 영역 구하기, Time: 244 ms, Memory: 27396 KB -BaekjoonHub

---
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.kt
@@ -0,0 +1,102 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.text.FieldPosition
+import java.util.ArrayDeque
+import java.util.ArrayList
+import java.util.LinkedList
+import java.util.Stack
+import java.util.StringTokenizer
+
+
+data class Info(val row: Int, val col: Int)
+
+fun main(){
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    var st = StringTokenizer(br.readLine())
+
+    val mapRow = st.nextToken().toInt()
+    val mapCol = st.nextToken().toInt()
+
+    val map = Array(mapRow) {BooleanArray(mapCol)}
+
+    val nr = intArrayOf(0, 0, 1, -1)
+    val nc = intArrayOf(1, -1, 0, 0)
+    val dq = ArrayDeque<Info>()
+
+    repeat(st.nextToken().toInt()){
+        st = StringTokenizer(br.readLine())
+        val sCol = st.nextToken().toInt() // 4
+        val sRow = st.nextToken().toInt() // 0
+        val eCol = st.nextToken().toInt()-1
+        val eRow = st.nextToken().toInt()-1
+
+        val start = Info(sRow, sCol)
+        val end = Info(eRow, eCol)
+        val tempMap = Array(mapRow) {BooleanArray(mapCol)}
+
+        tempMap[sRow][sCol] = true
+        dq.add(start)
+        while(!dq.isEmpty()){
+            val now = dq.pollFirst()
+            for(i in 0 .. 3){
+                val nextRow = now.row + nr[i]
+                val nextCol = now.col + nc[i]
+
+                if(nextRow < 0 || nextCol < 0 || nextRow >= mapRow || nextCol >= mapCol) continue
+                if(nextRow < start.row || nextCol < start.col || nextRow > end.row || nextCol > end.col) continue
+                if(tempMap[nextRow][nextCol]) continue
+                tempMap[nextRow][nextCol] = true
+                dq.addLast(Info(nextRow, nextCol))
+            }
+        }
+
+        for(row in 0 .. tempMap.size-1){
+            for(col in 0 .. tempMap[row].size-1){
+                if(tempMap[row][col]) map[row][col] = true
+            }
+        }
+    }
+
+    var count = 0
+    var result = ArrayList<Int>()
+
+    for(row in 0 .. map.size-1){
+        for(col in 0 .. map[row].size-1){
+            if(map[row][col]) continue
+            count++
+            dq.add(Info(row, col))
+            var area = 1
+
+            while(!dq.isEmpty()){
+                val now = dq.pollFirst()
+                map[now.row][now.col] = true
+
+                for(i in 0 .. 3){
+                    val nextRow = now.row + nr[i]
+                    val nextCol = now.col + nc[i]
+
+                    if(nextRow < 0 || nextCol < 0 || nextRow >= mapRow || nextCol >= mapCol) continue
+                    if(map[nextRow][nextCol]) continue
+
+                    map[nextRow][nextCol] = true
+                    area++
+                    dq.addLast(Info(nextRow, nextCol))
+                }
+            }
+            result.add(area)
+        }
+    }
+
+    bw.appendLine("$count")
+    result.sorted().forEach {
+        bw.append("$it ")
+    }
+
+    bw.flush()
+    bw.close()
+}
+
