8b9cf0daaad6ceeb705e2e689273a126cadf8e62
Lst-1995-kotlin <120715142+Lst-1995-kotlin@users.noreply.github.com>
2023-07-28 23:42:26 +0900

    [Silver IV] Title: 보물, Time: 140 ms, Memory: 19584 KB -BaekjoonHub

--- 백준/Silver/1026. 보물/보물.kt
+++ 백준/Silver/1026. 보물/보물.kt
@@ -13,38 +13,13 @@ fun main(){
     st = StringTokenizer(br.readLine())
     var Aarr = IntArray(S){st.nextToken().toInt()}.sorted()
     st = StringTokenizer(br.readLine())
-    var Barr = IntArray(S){st.nextToken().toInt()}.sorted()
+    var Barr = IntArray(S){st.nextToken().toInt()}.sortedDescending()
 
     var sum = 0
-
-    // A,B 오름차순
+    
     for(i in 0 .. Aarr.size-1){
         sum += Aarr[i]*Barr[i]
     }
-
-    // A: 오름, B: 내림
-    Barr = Barr.reversed()
-    var sum2 = 0
-    for(i in 0 .. Aarr.size-1){
-        sum2 += Aarr[i]*Barr[i]
-    }
-    sum = Math.min(sum, sum2)
-
-    // A: 내림, B: 내림
-    Aarr = Aarr.reversed()
-    var sum3 = 0
-    for(i in 0 .. Aarr.size-1){
-        sum3 += Aarr[i]*Barr[i]
-    }
-    sum = Math.min(sum, sum3)
-
-    // A: 내림, B: 올림
-    Barr = Barr.reversed()
-    var sum4 = 0
-    for(i in 0 .. Aarr.size-1){
-        sum4 += Aarr[i]*Barr[i]
-    }
-    sum = Math.min(sum, sum4)
 
     bw.append("$sum")
 
