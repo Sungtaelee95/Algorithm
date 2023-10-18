0898995e4c3c613b47161b68c39371e8fd33c71d
Lst-1995-kotlin <120715142+Lst-1995-kotlin@users.noreply.github.com>
2023-10-18 22:14:47 +0900

    [Gold II] Title: 계보 복원가 호석, Time: 2856 ms, Memory: 197616 KB, Score: 19 point -BaekjoonHub

--- 백준/Gold/21276. 계보 복원가 호석/계보 복원가 호석.kt
+++ 백준/Gold/21276. 계보 복원가 호석/계보 복원가 호석.kt
@@ -12,14 +12,20 @@ fun main(){
 
     val N = st.nextToken().toInt()
 
-    val humanMap = HashMap<String, MutableList<String>>() // 동네 사람들
-    val degree = HashMap<String, Int>()
+    val parentByChild = HashMap<String, MutableList<String>>() // 자식 후보 리스트
+    val parentBydegree = HashMap<String, Int>()
+
+    val parentByChildReverse = HashMap<String, MutableList<String>>()
+    val parentByDegreeReverse = HashMap<String, Int>()
 
     st = StringTokenizer(br.readLine())
     repeat(N){
         val human = st.nextToken()
-        humanMap[human] = mutableListOf()
-        degree[human] = 0
+        parentByChild[human] = mutableListOf()
+        parentBydegree[human] = 0
+
+        parentByChildReverse[human] = mutableListOf()
+        parentByDegreeReverse[human] = 0
     }
 
     st = StringTokenizer(br.readLine())
@@ -28,33 +34,53 @@ fun main(){
         st = StringTokenizer(br.readLine())
         val child = st.nextToken()
         val parent = st.nextToken()
-        humanMap[parent]!!.add(child)
-        degree[child] = degree[child]!! + 1
+        parentByChild[parent]!!.add(child)
+        parentBydegree[child] = parentBydegree[child]!! + 1
+
+        parentByChildReverse[parent]!!.add(child)
+        parentByDegreeReverse[child] = parentByDegreeReverse[child]!! + 1
+
     }
+
+    val resultMap = HashMap<String, MutableList<String>>()
 
     var rootCount = 0
     val sb = StringBuilder()
     val dq = ArrayDeque<String>()
 
-    val resultMap = HashMap<String, MutableList<String>>()
-
-    degree.forEach{
-        if(it.value == 0){
-            sb.append("${it.key} ")
-            dq.addLast(it.key)
+    parentBydegree.keys.sorted().forEach{
+        if(parentBydegree[it] == 0){
+            sb.append("$it ")
+            dq.addLast(it)
             rootCount++
         }
-        resultMap[it.key] = mutableListOf()
+        resultMap[it] = mutableListOf()
     }
     bw.appendLine("$rootCount")
-    bw.appendLine(sb.toString())
+    bw.appendLine(sb)
     sb.clear()
 
-    while (!dq.isEmpty()){
-        val parent = dq.poll()
-        humanMap[parent]!!.forEach {
-            degree[it] = degree[it]!! - 1
-            if(degree[it] == 0){
+    while (!dq.isEmpty()){ // 정방향
+        val parent = dq.pollFirst()
+        parentByChild[parent]!!.sorted().forEach {
+            parentBydegree[it] = parentBydegree[it]!! - 1
+            if(parentBydegree[it]!! <= 0){
+                resultMap[parent]!!.add(it)
+                dq.addLast(it)
+            }
+        }
+    }
+
+    parentByDegreeReverse.keys.sortedDescending().forEach{
+        if(parentByDegreeReverse[it] == 0){
+            dq.addLast(it)
+        }
+    }
+    while (!dq.isEmpty()){ // 역방향
+        val parent = dq.pollFirst()
+        parentByChildReverse[parent]!!.sortedDescending().forEach {
+            parentByDegreeReverse[it] = parentByDegreeReverse[it]!! - 1
+            if(parentByDegreeReverse[it]!! <= 0){
                 resultMap[parent]!!.add(it)
                 dq.addLast(it)
             }
@@ -62,8 +88,8 @@ fun main(){
     }
 
     resultMap.keys.sorted().forEach {
-        bw.append("$it ${resultMap[it]!!.size}")
-        resultMap[it]!!.sorted().forEach { child ->
+        bw.append("$it ${resultMap[it]!!.distinct().size}")
+        resultMap[it]!!.sorted().distinct().forEach { child ->
             bw.append(" $child")
         }
         bw.appendLine()
