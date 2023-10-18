55d8ce7218c6da764c55c2a582fa0854b44bba20
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2023-10-18 23:02:10 +0900

    Style: 포맷팅 수정

--- 백준/Gold/21276. 계보 복원가 호석/계보 복원가 호석.kt
+++ 백준/Gold/21276. 계보 복원가 호석/계보 복원가 호석.kt
@@ -13,7 +13,7 @@ fun main(){
     val N = st.nextToken().toInt()
 
     val parentByChild = HashMap<String, MutableList<String>>() // 자식 후보 리스트
-    val parentBydegree = HashMap<String, Int>()
+    val parentByDegree = HashMap<String, Int>()
 
     val parentByChildReverse = HashMap<String, MutableList<String>>()
     val parentByDegreeReverse = HashMap<String, Int>()
@@ -22,7 +22,7 @@ fun main(){
     repeat(N){
         val human = st.nextToken()
         parentByChild[human] = mutableListOf()
-        parentBydegree[human] = 0
+        parentByDegree[human] = 0
 
         parentByChildReverse[human] = mutableListOf()
         parentByDegreeReverse[human] = 0
@@ -35,61 +35,59 @@ fun main(){
         val child = st.nextToken()
         val parent = st.nextToken()
         parentByChild[parent]!!.add(child)
-        parentBydegree[child] = parentBydegree[child]!! + 1
+        parentByDegree[child] = parentByDegree[child]!! + 1
 
         parentByChildReverse[parent]!!.add(child)
         parentByDegreeReverse[child] = parentByDegreeReverse[child]!! + 1
 
     }
 
-    val resultMap = HashMap<String, MutableList<String>>()
+    val resultMap = HashMap<String, MutableSet<String>>()
 
     var rootCount = 0
     val sb = StringBuilder()
     val dq = ArrayDeque<String>()
+    val dq2 = ArrayDeque<String>()
 
-    parentBydegree.keys.sorted().forEach{
-        if(parentBydegree[it] == 0){
+    parentByDegree.keys.sorted().forEach{
+        if(parentByDegree[it] == 0){
             sb.append("$it ")
             dq.addLast(it)
+            dq2.addFirst(it)
             rootCount++
         }
-        resultMap[it] = mutableListOf()
+        resultMap[it] = mutableSetOf()
     }
+
     bw.appendLine("$rootCount")
     bw.appendLine(sb)
-    sb.clear()
 
-    while (!dq.isEmpty()){ // 정방향
-        val parent = dq.pollFirst()
-        parentByChild[parent]!!.sorted().forEach {
-            parentBydegree[it] = parentBydegree[it]!! - 1
-            if(parentBydegree[it]!! <= 0){
-                resultMap[parent]!!.add(it)
-                dq.addLast(it)
+    while (!dq.isEmpty() && !dq2.isEmpty()){ // 정방향
+        if(!dq.isEmpty()){
+            val parent = dq.pollFirst()
+            parentByChild[parent]!!.sorted().forEach {
+                parentByDegree[it] = parentByDegree[it]!! - 1
+                if(parentByDegree[it]!! <= 0){
+                    resultMap[parent]!!.add(it)
+                    dq.addLast(it)
+                }
             }
         }
-    }
-
-    parentByDegreeReverse.keys.sortedDescending().forEach{
-        if(parentByDegreeReverse[it] == 0){
-            dq.addLast(it)
-        }
-    }
-    while (!dq.isEmpty()){ // 역방향
-        val parent = dq.pollFirst()
-        parentByChildReverse[parent]!!.sortedDescending().forEach {
-            parentByDegreeReverse[it] = parentByDegreeReverse[it]!! - 1
-            if(parentByDegreeReverse[it]!! <= 0){
-                resultMap[parent]!!.add(it)
-                dq.addLast(it)
+        if(!dq2.isEmpty()){
+            val parent = dq2.pollFirst()
+            parentByChildReverse[parent]!!.sortedDescending().forEach {
+                parentByDegreeReverse[it] = parentByDegreeReverse[it]!! - 1
+                if(parentByDegreeReverse[it]!! <= 0){
+                    resultMap[parent]!!.add(it)
+                    dq2.addLast(it)
+                }
             }
         }
     }
 
     resultMap.keys.sorted().forEach {
-        bw.append("$it ${resultMap[it]!!.distinct().size}")
-        resultMap[it]!!.sorted().distinct().forEach { child ->
+        bw.append("$it ${resultMap[it]!!.size}")
+        resultMap[it]!!.sorted().forEach { child ->
             bw.append(" $child")
         }
         bw.appendLine()
