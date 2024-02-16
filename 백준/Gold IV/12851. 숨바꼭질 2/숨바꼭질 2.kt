369dd54022ee4d0742cd2308e58d1b395c175706
Lst-1995-kotlin <120715142+Lst-1995-kotlin@users.noreply.github.com>
2024-02-16 17:15:01 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 500 ms, Memory: 123104 KB -BaekjoonHub

--- 백준/Gold IV/12851. 숨바꼭질 2/숨바꼭질 2.kt
+++ 백준/Gold IV/12851. 숨바꼭질 2/숨바꼭질 2.kt
@@ -10,28 +10,27 @@ fun main() {
     val N = st.nextToken().toInt()
     val K = st.nextToken().toInt()
 
-    val visited = BooleanArray(200_002)
+    val visited = IntArray(200_001) { Int.MAX_VALUE }
     val dq = ArrayDeque<Info>()
     dq.add(Info(N, 0))
-    var minCount = Int.MAX_VALUE
     var count = 0
     while (dq.isNotEmpty()) {
         val now = dq.removeFirst()
         if (now.position == K) {
-            if (now.count < minCount) {
-                minCount = now.count
+            if (now.count < visited[now.position]) {
+                visited[now.position] = now.count
                 count = 1
                 continue
             }
-            if (now.count == minCount) count++
+            if (now.count == visited[now.position]) count++
             continue
         }
-        visited[now.position] = true
-        if (now.position+1 in 0..200_001 && !visited[now.position+1]) dq.addLast(Info(now.position+1, now.count+1))
-        if (now.position-1 in 0..200_001 && !visited[now.position-1]) dq.addLast(Info(now.position-1, now.count+1))
-        if (now.position*2 in 0..200_001 && !visited[now.position*2]) dq.addLast(Info(now.position*2, now.count+1))
+        visited[now.position] = now.count
+        if (now.position+1 in 0..200_000 && now.count+1 <= visited[now.position+1]) dq.addLast(Info(now.position+1, now.count+1))
+        if (now.position-1 in 0..200_000 && now.count+1 <= visited[now.position-1]) dq.addLast(Info(now.position-1, now.count+1))
+        if (now.position*2 in 0..200_000 && now.count+1 <= visited[now.position*2]) dq.addLast(Info(now.position*2, now.count+1))
     }
-    bw.append("$minCount\n$count")
+    bw.append("${visited[K]}\n$count")
 
     bw.flush()
     bw.close()
