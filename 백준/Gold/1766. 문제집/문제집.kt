7e5241b5edb33e5be281760b2e6d9cd0c685bbf7
lst-1995 <120715142+Lst-1995-kotlin@users.noreply.github.com>
2024-09-13 01:05:56 +0900

    [Gold II] Title: 문제집, Time: 456 ms, Memory: 53924 KB -BaekjoonHub

--- 백준/Gold/1766. 문제집/문제집.kt
+++ 백준/Gold/1766. 문제집/문제집.kt
@@ -5,7 +5,6 @@ import java.io.OutputStreamWriter
 import java.util.*
 
 
-
 fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
     val bw = BufferedWriter(OutputStreamWriter(System.out))
@@ -14,18 +13,18 @@ fun main() {
     val n = st.nextToken().toInt()
     val m = st.nextToken().toInt()
 
-    val count = IntArray(n+1)
-    val arr = Array(n+1) { mutableListOf<Int>() }
+    val count = IntArray(n + 1)
+    val arr = Array(n + 1) { mutableListOf<Int>() }
     repeat(m) {
-        val (e, h) = br.readLine().split(" ").map { it.toInt() }
+        st = StringTokenizer(br.readLine())
+        val e = st.nextToken().toInt()
+        val h = st.nextToken().toInt()
         count[h]++
         arr[e].add(h)
     }
     val pq = PriorityQueue<Int>()
-    for (i in count.indices) {
-        if (i != 0) {
-            if (count[i] == 0) pq.add(i)
-        }
+    for (i in 1..n) {
+        if (count[i] == 0) pq.add(i)
     }
     while (pq.isNotEmpty()) {
         val num = pq.poll()
