6677fbc91394063a5993772406526dfd2c62b1b6
sungtae-lee <120715142+Sungtaelee95@users.noreply.github.com>
2025-02-05 00:22:10 +0900

    [Silver V] Title: 올림픽, Time: 252 ms, Memory: 27028 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.kt
+++ 백준/Silver/8979. 올림픽/올림픽.kt
@@ -4,18 +4,14 @@ import java.util.*
 fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
     val bw = BufferedWriter(OutputStreamWriter(System.out))
-    
+
     val (n, k) = br.readLine().split(" ").map{ it.toInt() }
-    
+
     val pq = PriorityQueue<Info>(
         Comparator{ o1, o2 ->
             if (o1.g == o2.g) {
                 if (o1.s == o2.s) {
-                    if (o1.b == o2.b) {
-                        o1.num.compareTo(o2.num)
-                    } else {
-                        o2.b.compareTo(o1.b)
-                    }
+                    o2.b.compareTo(o1.b)
                 } else {
                     o2.s.compareTo(o1.s)
                 }
@@ -24,33 +20,24 @@ fun main() {
             }
         }
     )
+    var target = Info(0,0,0)
     repeat(n) {
         val (num,g,s,b) = br.readLine().split(" ").map{it.toInt()}
-        pq.add(Info(num,g,s,b))
+        pq.add(Info(g,s,b))
+        if (num == k) target = Info(g,s,b)
     }
-    var last = Info(0,1_000_001,0,0)
-    var rank = 0
-    var equals = 0
+
+    var rank = 1
     while(pq.isNotEmpty()) {
         val now = pq.poll()
-        if (last.g == now.g && last.s == now.s && last.b == now.b) {
-            equals++
-        } else {
-            if (equals == 0){
-                 rank++ 
-            }else {
-                rank += equals
-                equals = 0
-            }
-        }
-        if (now.num == k) {
+        if (now == target) {
             bw.append("$rank")
             break
         }
-        last = now
+        rank++
     }
     bw.flush()
     bw.close()
 }
 
-data class Info(val num: Int, val g: Int, val s: Int, val b: Int)
+data class Info(val g: Int, val s: Int, val b: Int)
