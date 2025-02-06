0464b0b016da04061f8b35dfeaccbdfadd836c95
sungtae-lee <120715142+Sungtaelee95@users.noreply.github.com>
2025-02-07 01:14:59 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1100 ms, Memory: 184800 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.kt
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.kt
@@ -1,30 +1,20 @@
-import java.util.StringTokenizer
+import java.io.*
 
-fun main() {
-    var st = StringTokenizer(readln())
-    val N = st.nextToken().toInt()
-    val M = st.nextToken().toInt()
-    val arr = Array(N+1){IntArray(N+1){0}}
-
-
-    for(i in 1 .. N){
-        st = StringTokenizer(readln())
-        for(j in 1 .. N){
-            arr[i][j] = arr[i][j-1]+ arr[i-1][j] - arr[i-1][j-1] + st.nextToken().toInt()
+fun main() { 
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+    val (n, m) = br.readLine().split(" ").map{ it.toInt() }
+    val dp = Array(n+1) { IntArray(n+1) }
+    for (i in 1 .. n) {
+        val input = br.readLine().split(" ").map{ it.toInt() }
+        for (j in 1 .. n) {
+            dp[i][j] = dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1] + input[j-1]
         }
     }
-    var sb = StringBuilder()
-    repeat(M){
-        st = StringTokenizer(readln())
-        var x1 = st.nextToken().toInt()
-        var y1 = st.nextToken().toInt()
-        var x2 = st.nextToken().toInt()
-        var y2 = st.nextToken().toInt()
-
-        sb.appendLine(arr[x2][y2] -arr[x1-1][y2] - arr[x2][y1-1] + arr[x1-1][y1-1])
-
+    repeat(m) {
+        val (x1,y1,x2,y2) = br.readLine().split(" ").map{ it.toInt() }
+        bw.appendLine("${dp[x2][y2] - dp[x1-1][y2] - dp[x2][y1-1] + dp[x1-1][y1-1]}")
     }
-
-    println(sb)
-
+    bw.flush()
+    bw.close()
 }
