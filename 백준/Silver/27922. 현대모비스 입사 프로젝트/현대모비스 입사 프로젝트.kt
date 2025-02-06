1118c5eed39ea18d31ab791938e2d2ddcdddfab9
sungtae-lee <120715142+Sungtaelee95@users.noreply.github.com>
2025-02-07 00:33:55 +0900

    [Silver III] Title: 현대모비스 입사 프로젝트, Time: 1516 ms, Memory: 234228 KB -BaekjoonHub

--- 백준/Silver/27922. 현대모비스 입사 프로젝트/현대모비스 입사 프로젝트.kt
+++ 백준/Silver/27922. 현대모비스 입사 프로젝트/현대모비스 입사 프로젝트.kt
@@ -14,9 +14,9 @@ fun main() {
         movies[1].add(Movie(a,c))
         movies[2].add(Movie(b,c))
     }
-    val sum1 = movies[0].sortedBy {it.x + it.y}.reversed().slice(0 until k).sumOf{it.x + it.y}
-    val sum2 = movies[1].sortedBy {it.x + it.y}.reversed().slice(0 until k).sumOf{it.x + it.y}
-    val sum3 = movies[2].sortedBy {it.x + it.y}.reversed().slice(0 until k).sumOf{it.x + it.y}
+    val sum1 = movies[0].sortedByDescending {it.x + it.y}.slice(0 until k).sumOf{it.x + it.y}
+    val sum2 = movies[1].sortedByDescending {it.x + it.y}.slice(0 until k).sumOf{it.x + it.y}
+    val sum3 = movies[2].sortedByDescending {it.x + it.y}.slice(0 until k).sumOf{it.x + it.y}
 
     bw.append("${maxOf(sum1, maxOf(sum2, sum3))}")
 
