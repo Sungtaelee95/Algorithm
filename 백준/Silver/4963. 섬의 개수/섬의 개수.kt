0b6146861d6f3aaff9cdee6f1dcf9980d4c62fe7
sungtae-lee <120715142+Sungtaelee95@users.noreply.github.com>
2025-06-19 22:06:07 +0900

    [Silver II] Title: 섬의 개수, Time: 232 ms, Memory: 27212 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.kt
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.kt
@@ -11,17 +11,17 @@ fun main() {
         val dq = ArrayDeque<Node>()
         for (r in 0 until h) {
             for (c in 0 until w) {
-                if (!visited[r][c] && map[r][c] == 1) {
-                    visited[r][c] = true
-                    cnt++
-                    dq.add(Node(r, c))
-                }
+                if (visited[r][c] || map[r][c] == 0) continue
+                cnt++
+                dq.add(Node(r, c))
+                visited[r][c] = true
                 while (dq.isNotEmpty()) {
                     val now = dq.removeFirst()
                     for (i in 0..7) {
                         val nr = now.row + rows[i]
                         val nc = now.col + cols[i]
-                        if (nr in 0 until h && nc in 0 until w && map[nr][nc] == 1 && !visited[nr][nc]) {
+                        if (nr < 0 || nc < 0 || nr >= h || nc >= w || map[nr][nc] == 0) continue
+                        if (!visited[nr][nc]) {
                             dq.addLast(Node(nr, nc))
                             visited[nr][nc] = true
                         }
